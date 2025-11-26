dc3d58de036be72ee8eee15853eb5ebc77740ebc
kiraventom <kiraventom@gmail.com>
2025-11-26 23:46:02 +0400

    Init

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Telegram.Bot;
+
+public class Program
+{
+    public static async void Main(string[] args)
+    {
+        string token = "8535580400:AAHnZBXUIQMmNyvyOua10FIrEI1syIE0uHo";
+        TelegramBotClient client = new TelegramBotClient(token);
+
+        long chatId = 493229987;
+        string text = "сука ебучая мошка блядь";
+        await client.SendMessage(chatId, text);
+    }
+}
