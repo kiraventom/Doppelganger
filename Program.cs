9f052b6722eff31ffdc5e3a39693418c4c9f82af
TTReMagi <Test@list.ru>
2025-11-30 00:08:01 +0300

    вынес токен в отдельный файл

--- Program.cs
+++ Program.cs
@@ -2,9 +2,11 @@
 
 public class Program
 {
-    public static async void Main(string[] args)
+    public static async Task Main()
     {
-        string token = "8535580400:AAHnZBXUIQMmNyvyOua10FIrEI1syIE0uHo";
+        
+        string[] lines = File.ReadAllLines("C:/Users/Nikita/Documents/Doppelganger/токен бота.txt");
+        string token = lines[0];
         TelegramBotClient client = new TelegramBotClient(token);
 
         long chatId = 493229987;
